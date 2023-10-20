c2642a3ede44c0b9ebeff0db5ebcb774b99a2751
Patrik-07 <patrik.santa07@gmail.com>
2023-10-20 17:01:30 +0200

    Update service and controller

--- src/main/kotlin/mbh/primesearch/controllers/PrimeSearchController.kt
+++ src/main/kotlin/mbh/primesearch/controllers/PrimeSearchController.kt
@@ -1,22 +1,23 @@
 package mbh.primesearch.controllers
 
+import mbh.primesearch.services.PrimeSearchService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/search")
-private class PrimeSearchController {
+private class PrimeSearchController(private val primeSearchService: PrimeSearchService) {
     @PostMapping("/start")
     fun startSearch(@RequestParam(required = false, defaultValue = "1") threadsCount: Number) {
-
+        primeSearchService.startSearch()
     }
 
     @GetMapping("/list")
-    fun listPrimes(): List<Number> {
-        return listOf(1, 2, 3)
+    fun listPrimes(): List<Long> {
+        return primeSearchService.collectedPrimeNumbers
     }
 
     @GetMapping("/stop")
     fun stopSearch() {
-
+        primeSearchService.stopSearch()
     }
 }
--- src/main/kotlin/mbh/primesearch/services/PrimeSearchService.kt
+++ src/main/kotlin/mbh/primesearch/services/PrimeSearchService.kt
@@ -1,4 +1,18 @@
 package mbh.primesearch.services
 
+import org.springframework.stereotype.Service
+
+@Service
 class PrimeSearchService {
+    private val primeNumbers = mutableListOf<Long>()
+
+    val collectedPrimeNumbers: List<Long> get() = primeNumbers
+
+    fun startSearch() {
+
+    }
+
+    fun stopSearch() {
+
+    }
 }
